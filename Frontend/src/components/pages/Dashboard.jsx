e4e16b3b2490cc56947516201b7b9a515f286ffe
sachin00976 <162875619+sachin00976@users.noreply.github.com>
2025-04-05 15:29:01 +0530

    fix: update user profile routes and improve form handling in EditProfile and Register components

--- Frontend/src/components/pages/Dashboard.jsx
+++ Frontend/src/components/pages/Dashboard.jsx
@@ -87,12 +87,12 @@ function Dashboard() {
     Create New Blog
   </button>):null}
 
-  {/* <button
+  <button
     onClick={() => navigate('/editProfile')}
-    className="bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-200"
+    className="bg-purple-600 hover:bg-purple-500 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-200"
   >
     Edit Profile
-  </button> */}
+  </button>
   </div>
   
       {/* User Blogs */}
